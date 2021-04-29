aa4e4236ca2d63b5536d9ed26ca9fddb985063d0
jy-hua <jhuang17@chicagobooth.edu>
2021-04-29 15:49:31 +0000

    all tests passing

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,11 +8,11 @@ class StudentsController < ApplicationController
   end
 
   def show
-    the_id = params.fetch("id")
+    the_id = params.fetch("path_id")
 
-    matching_students = Enrollment.where({ :id => the_id })
+    matching_students = Enrollment.where({ :student_id => the_id })
 
-    @the_student = matching_students.at(0)
+    @the_student = Student.where({ :id => matching_students.at(0).student_id }).first
 
     render({ :template => "students/show.html.erb" })
   end
